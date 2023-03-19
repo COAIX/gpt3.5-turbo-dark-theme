e25fce64716cdd5ab62c2aff299c506276563731
COAIX <84883582+COAIX@users.noreply.github.com>
2023-03-19 18:54:20 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,87 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/edge";
-import unocss from "unocss/astro";
-import { presetUno, presetIcons, presetAttributify, presetTypography } from "unocss";
-import solidJs from "@astrojs/solid-js";
-import node from '@astrojs/node';
+import { defineConfig } from "astro/config"
+import vercel from "@astrojs/vercel/edge"
+import node from "@astrojs/node"
+import netlify from "@astrojs/netlify/edge-functions"
+import cloudflare from "@astrojs/cloudflare"
+import unocss from "unocss/astro"
+import {
+  presetUno,
+  presetIcons,
+  presetAttributify,
+  presetTypography
+} from "unocss"
+import solidJs from "@astrojs/solid-js"
+import AstroPWA from "@vite-pwa/astro"
+
+const adapter = () => {
+  if (process.env.VERCEL) {
+    return vercel()
+  } else if (process.env.NETLIFY) {
+    return netlify()
+  } else if (process.env.CLOUDFLARE) {
+    // cloudflare 无法提供 node18 环境，所以目前无法正常运行。
+    return cloudflare()
+  } else {
+    return node({
+      mode: "standalone"
+    })
+  }
+}
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              const arr = id.toString().split("node_modules/")[1].split("/");
-              if (arr[1].includes("markdown")) return "__markdown";
-              if (arr[1].includes("highlight")) return "__highlight";
-              return "__vendor";
-            }
+  integrations: [
+    unocss({
+      presets: [
+        presetAttributify(),
+        presetUno(),
+        presetTypography({
+          cssExtend: {
+            ":not(pre) > code::before,:not(pre) > code::after": ""
           }
-        }
+        }),
+        presetIcons()
+      ]
+    }),
+    solidJs(),
+    AstroPWA({
+      base: "/",
+      scope: "/",
+      includeAssets: ["favicon.svg"],
+      manifest: {
+        name: "ChatGPT",
+        lang: "zh-cn",
+        short_name: "ChatGPT",
+        background_color: "#f6f8fa",
+        icons: [
+          {
+            src: "192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      },
+      workbox: {
+        navigateFallback: "/404",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt,astro}"]
       }
-    }
-  },
-  integrations: [unocss({
-    presets: [presetAttributify(), presetUno(), presetTypography({
-      cssExtend: {
-        ":not(pre) > code::before,:not(pre) > code::after": ""
-      }
-    }), presetIcons()]
-  }), solidJs()],
+    })
+  ],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
-});
+  adapter: adapter()
+})
